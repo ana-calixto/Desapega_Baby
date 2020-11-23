f46efbc0683210bbcb005a3a0a90f119acc5037e
Ana Calixto <aninhacalixto@gmail.com>
2020-11-23 17:21:34 -0300

    user-controller added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :brand, presence: true
+  validates :description, presence: true
+  validates :price, presence: true  
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :deals
 end
