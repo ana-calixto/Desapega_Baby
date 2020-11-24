5385627820116a519a3cf5b2e5153863de6dc5fa
Lincoln Marcolongo <mlincoln1205@gmail.com>
2020-11-24 21:23:52 +0100

    Product its not displayed after deal

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,42 +1,42 @@
 class ProductsController < ApplicationController
-  before_action :product_find, only: [:show, :edit, :update, :destroy] 
+  before_action :product_find, only: [:show, :edit, :update, :destroy]
 
-  def index 
-    @products = Product.all 
+  def index
+    @products = Product.all.where(available: true)
   end
 
-  def show 
+  def show
   end
 
   def new
     @product = Product.new
   end
 
-  def create 
+  def create
     @product = Product.new(product_params)
     @product.owner = current_user
     if @product.save
       redirect_to product_path(@product)
-    else 
-      render :new 
+    else
+      render :new
     end
   end
 
   def edit
   end
 
-  def update 
+  def update
     @product = Product.find(params[:id])
       if @product.update(product_params)
         redirect_to product_path(@product)
-      else 
+      else
         render :edit
       end
-  end 
-  
+  end
+
   def destroy
     @product.destroy
-    redirect_to products_path 
+    redirect_to products_path
   end
 
   private
@@ -45,7 +45,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  def product_params 
+  def product_params
     params.require(:product).permit(:name, :description, :brand, :price)
   end
 end
