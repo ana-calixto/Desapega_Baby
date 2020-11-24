37a64f8c3fc73b02eb2f6d974c321b890cb9ca79
Ana Calixto <aninhacalixto@gmail.com>
2020-11-24 17:11:00 -0300

    update see products if not logged in

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :product_find, only: [:show, :edit, :update, :destroy] 
 
   def index 
