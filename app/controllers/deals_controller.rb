8ee088315495f639fed5a4aa610b3a54c365bcc3
Ana Calixto <aninhacalixto@gmail.com>
2020-11-24 08:35:15 -0300

    add deal controller and deal routes

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,2 +1,23 @@
 class DealsController < ApplicationController
+  def index 
+    @deals = Deal.all 
+  end
+
+  def show
+    @deal = Deal.find(params[:id])
+  end
+
+  def new
+    @deal = Deal.new
+  end
+
+  def create
+    @deal = Deal.new
+    @product = Product.find(:id)
+    @deal.user = current_user
+    @deal.product = @product
+    @deal.save
+    redirect_to products_path
+  end
+
 end
