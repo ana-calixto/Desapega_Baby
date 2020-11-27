fe976bd497c961eebc91140657b962e0a95b165a
Pedro Eduardo <eduardototi@gmail.com>
2020-11-27 15:52:38 -0300

    ultimas alterações no app

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,7 @@ class ProfilesController < ApplicationController
   def show
     @profile = User.find(params[:id])
     @reviews = @profile.received_reviews
+    @fivestars = @reviews.select{ |r| r.raiting == 5 }
+    @fivestarspercent = (@fivestars.count / @reviews.count) *100
   end
 end
